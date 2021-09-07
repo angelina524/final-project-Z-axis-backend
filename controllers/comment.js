15035c416daf7ea5d5b50fd3edd047332c1ca095
Allen Liao <namo50095@gmail.com>
2021-09-07 14:14:26 +0800

    added comments path and commentControler

---
+++ controllers/comment.js
@@ -0,0 +1,89 @@
+const db = require('../models')
+const { Issue_Guest_Comment: Comment } = db
+
+const commentController = {
+  addComment: async (req, res) => {
+    const { nickname, content } = req.body
+    const { id } = req.params
+    // guestToken?
+    const guestToken = 'testToken123'
+    // todo: error handling
+    const comment = await Comment.create({
+      nickname,
+      content,
+      issueId: Number(id),
+      guestToken,
+    })
+    res.send({ comment })
+  },
+  deleteComment: async (req, res) => {
+    const { id } = req.params
+    // todo: error handling
+    await Comment.destroy({
+      where: {
+        id: Number(id),
+      },
+    })
+    res.send('deleteComment')
+  },
+  editComment: async (req, res) => {
+    const { nickname, content } = req.body
+    const { id } = req.params
+    // todo: error handling
+    const response = await Comment.update(
+      {
+        nickname,
+        content,
+      },
+      {
+        where: {
+          id: Number(id),
+        },
+      }
+    )
+    res.send({ response })
+  },
+  getAllComments: async (req, res) => {
+    // todo: error handling
+    const comments = await Comment.findAll()
+    res.send({ comments })
+  },
+  getOneComment: async (req, res) => {
+    const { id } = req.params
+    // todo: error handling
+    const comment = await Comment.findOne({
+      where: {
+        id: Number(id),
+      },
+    })
+    res.send({ comment })
+  },
+  editReply: async (req, res) => {
+    const { reply } = req.body
+    const { id } = req.params
+    const response = await Comment.update(
+      {
+        reply,
+        replyCreateAt: new Date(),
+      },
+      {
+        where: {
+          id: Number(id),
+        },
+      }
+    )
+    res.send({ response })
+  },
+  getAllReplies: async (req, res) => {
+    const { id } = req.params
+    const replies = await Comment.findAll({
+      where: {
+        issueId: Number(id),
+      },
+      attributes: ['reply', 'replyCreateAt'],
+    })
+    res.send({ replies })
+  },
+}
+
+module.exports = commentController
--- index.js
+++ index.js
@@ -5,23 +5,24 @@ const path = require('path')
 
 const userController = require('./controllers/user')
 const issueController = require('./controllers/issue')
+const commentController = require('./controllers/comment')
 
 const app = express()
 const port = process.env.PORT || 5001
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(flash())
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true
-}))
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+  })
+)
 
-function checkAdminPermission(req, res, next) {
-
-}
+function checkAdminPermission(req, res, next) {}
 
 // user
 app.post('/users/register', userController.register)
@@ -39,6 +40,16 @@ app.delete('/issues/:id', issueController.delete)
 app.patch('/issues/:id', issueController.patch)
 app.get('/issues', issueController.getAll)
 app.get('/issues/:id', issueController.getOne)
+
+// comment path
+app.post('/issue/:id/comments', commentController.addComment)
+app.delete('/comments/:id', commentController.deleteComment)
+app.patch('/comments/:id', commentController.editComment)
+app.get('/comments', commentController.getAllComments)
+app.get('/comments/:id', commentController.getOneComment)
+// reply path
+app.patch('/comments/:id/replies', commentController.editReply)
+app.get('/issue/:id/replies', commentController.getAllReplies)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
