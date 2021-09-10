e648ab23a331a978910e93a7eeb8fc4c09a3d74b
Benben <z2266109@gmail.com>
2021-09-10 12:47:40 +0800

    rebase dev / eslint setting

--- controllers/issue.js
+++ controllers/issue.js
@@ -2,31 +2,78 @@ const db = require('../models')
 const { Issue } = db
 
 const issueController = {
-  add: (req, res) => {
-    Issue.findAll().then((issue) => {
-      // TODO
+  add: async (req, res) => {
+    const { title, description, beginTime, finishTime } = req.body
+    // To discuss : userId storage
+    const { userId } = req.session
+    // To do : error handling
+    const issue = await Issue.create({
+      title,
+      description,
+      beginTime,
+      finishTime,
+      userId,
     })
+    res.send({ issue })
   },
-  delete: (req, res) => {
-    Issue.findOne().then((issue) => {
-      // TODO
-    })
+  delete: async (req, res) => {
+    const { id } = req.params
+    const { userId } = req.session
+    // To discuss hard or soft delete
+    await Issue.update(
+      {
+        isDelete: 1,
+      },
+      {
+        where: {
+          id: Number(id),
+          userId: Number(userId),
+        },
+      }
+    )
+    res.send('deleteIssue')
   },
-  patch: (req, res) => {
-    Issue.findOne().then((issue) => {
-      // TODO
-    })
+  patch: async (req, res) => {
+    const { title, description, beginTime, finishTime } = req.body
+    const { id } = res.params
+    const { userId } = req.session
+    const response = await Issue.update(
+      {
+        title,
+        description,
+        beginTime,
+        finishTime,
+      },
+      {
+        where: {
+          id: Number(id),
+          userId: Number(userId),
+        },
+      }
+    )
+    res.send({ response })
   },
-  getAll: (req, res) => {
-    Issue.findAll().then((issue) => {
-      // TODO
+  getAll: async (req, res) => {
+    const { userId } = req.userId
+    const issues = await Issue.findAll({
+      where: {
+        userId: Number(userId)
+      }
     })
+    res.send({ issues })
   },
-  getOne: (req, res) => {
-    Issue.findOne().then((issue) => {
-      // TODO
+  getOne: async (req, res) => {
+    const { userId } = req.session
+    const { id } = req.params
+    // To do : error handling
+    const singleIssue = await Issue.findOne({
+      where: {
+        id: Number(id),
+        userId: Number(userId),
+      }
     })
-  }
+    res.send({ singleIssue })
+  },
 }
 
 module.exports = issueController
