d3ef9e4335249c5d54705f4a4884c2886fa3efb9
Allen Liao <namo50095@gmail.com>
2021-10-09 23:16:46 +0800

    add cors options

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ const port = PORT || 5001
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
+app.options('*', cors())
 
 // routers
 app.use('/users', userRouter)
